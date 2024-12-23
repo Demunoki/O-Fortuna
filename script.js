1acf6bc1f77842fa5074142429f96296264f4dd0
Dominic Locks <dominic.locks@gmail.com>
2024-12-23 05:27:14 +0100

    rerollOnce

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const diceConfig = {
-    4: { numRolls: 5, targetAverage: 0.5,previous:[] },
-    6: { numRolls: 5, targetAverage: 1.5,previous:[] },
-    8: { numRolls: 5, targetAverage: 2.5,previous:[] },
-    10: { numRolls: 5, targetAverage: 3.5,previous:[] },
-    12: { numRolls: 5, targetAverage: 4.5 ,previous:[]},
-    20: { numRolls: 5, targetAverage: 8.5,previous:[] },
-    100: { numRolls: 5, targetAverage: 48.5,previous:[] }
+    4: { numRolls: 5, targetAverage: 2.5,previous:[] },
+    6: { numRolls: 5, targetAverage: 3.5,previous:[] },
+    8: { numRolls: 5, targetAverage: 4.5,previous:[] },
+    10: { numRolls: 5, targetAverage: 5.5,previous:[] },
+    12: { numRolls: 5, targetAverage: 6.5 ,previous:[]},
+    20: { numRolls: 5, targetAverage: 10.5,previous:[] },
+    100: { numRolls: 5, targetAverage: 50.5,previous:[] }
 };
 
 
@@ -13,18 +13,18 @@ for (const dieType in diceConfig) {
     const config = diceConfig[dieType];
     config.previous = Array(config.numRolls).fill(config.targetAverage);
 }
+    function getARandom(dieType){
+        const config=diceConfig[dieType]
+        const randomNumber = Math.floor(Math.random() * dieType) + 1;
+        const average=(config.previous.reduce((a, b) => a + b, 0)+randomNumber)/config.numRolls;
+        return {randomNumber,average}}
 
 function rollDice(dieType) {
     const config = diceConfig[dieType];
-    let randomNumber;
-    let average;
-
-    // Continue generating random numbers until the average is above the target
-    do {
-        randomNumber = Math.floor(Math.random() * dieType) + 1;
-        average=(config.previous.reduce((a, b) => a + b, 0)+randomNumber)/config.numRolls;
-        // If the average is below the target, reroll
-    } while (average < config.targetAverage);
+    let {randomNumber, average}=getARandom(dieType)
+    if(average<config.targetAverage){
+        ({randomNumber, average}=getARandom(dieType))
+    }
     config.previous.shift()
     config.previous.push(randomNumber)
     document.getElementById('total').innerHTML=`<strong> ${randomNumber}</strong>`
